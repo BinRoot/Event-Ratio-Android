0eca67eb492490c072b10e2664850b99d61f0e1b
Jasdev <js2wt@virginia.edu>
2012-07-21 13:42:29 -0700

    Setting up project

---
+++ src/com/hackathon/eventratio/EventRatioActivity.java
@@ -0,0 +1,13 @@
+package com.hackathon.eventratio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventRatioActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
